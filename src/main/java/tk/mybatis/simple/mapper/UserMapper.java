eff12c7a4c1b318dea0b44ed73f6fdc0566efdcc
张琦 <Qi.Zhang4@geely.com>
2019-05-24 13:41:18 +0800

    [ADD]增加第二章示例代码

--- src/main/java/tk/mybatis/simple/mapper/UserMapper.java
+++ src/main/java/tk/mybatis/simple/mapper/UserMapper.java
@@ -1,10 +1,17 @@
 package tk.mybatis.simple.mapper;
 
+import tk.mybatis.simple.model.SysUser;
+
 /**
  * @Auther: Qi.Zhang4
  * @Date: 2019/5/23 08:43
  * @Description:
  */
 public interface UserMapper {
-
+    /**
+     * 通过id查询用户
+     * @param id
+     * @return
+     */
+    SysUser selectById(Long id);
 }
