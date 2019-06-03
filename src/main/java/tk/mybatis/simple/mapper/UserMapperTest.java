2b07b892dffd5baec84f58020d0a10748e45ff82
张琦 <Qi.Zhang4@geely.com>
2019-06-03 13:44:00 +0800

    [ADD]增加第二章示例代码

--- src/main/java/tk/mybatis/simple/mapper/UserMapperTest.java
+++ src/main/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import tk.mybatis.simple.model.SysRole;
 import tk.mybatis.simple.model.SysUser;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -59,6 +60,37 @@ public class UserMapperTest extends BaseMapperTest {
             Assert.assertNotNull(roleList);
             Assert.assertTrue(roleList.size() > 0);
         } finally {
+            //不要忘记关闭sqlSession
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testInsert(){
+        SqlSession sqlSession = getSqlSession();
+        try{
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            //创建一个user对象
+            SysUser user = new SysUser();
+            user.setUserName("test1");
+            user.setUserPassword("123456");
+            user.setUserEmail("test@mybatis.tk");
+            user.setUserInfo("test info");
+            //正常情况下应该读入一张图片到byte数组中
+            user.setHeadImg(new byte[]{1,2,3});
+            user.setCreateTime(new Date());
+            //将新建的对象插入数据库中，特别注意这里的返回值result是执行的SQL影响的行数
+            int result = userMapper.insert(user);
+            //只插入1条数据
+            Assert.assertEquals(1, result);
+            //id为null，没有给id赋值，并且没有配置回写id的值
+            Assert.assertNull(user.getId());
+        } finally {
+            //为了不影响其他测试，这里选择回滚
+            //由于默认的sqlSessionFactory.openSession()是不自动提交的
+            //因此不手动执行commit也不会提交到数据库
+            sqlSession.rollback();
+            //不要忘记关闭sqlSession
             sqlSession.close();
         }
     }
