d72425a59cd8c206438bc95492e7623a1119a854
Qi.Zhang4 <Qi.Zhang4@geely.com>
2019-06-24 08:51:50 +0800

    [ADD]增加第三章示例代码

--- src/main/java/tk/mybatis/simple/mapper/RoleMapper.java
+++ src/main/java/tk/mybatis/simple/mapper/RoleMapper.java
@@ -1,5 +1,8 @@
 package tk.mybatis.simple.mapper;
 
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.simple.model.SysRole;
+
 /**
  * @Auther: Qi.Zhang4
  * @Date: 2019/5/23 08:43
@@ -7,4 +10,14 @@ package tk.mybatis.simple.mapper;
  */
 public interface RoleMapper {
 
+    /**
+     * 以下两种方式都可以
+     * @param id
+     * @return
+     */
+    @Select({"select id, role_name roleName, enabled, create_by createBy, create_time createTime ",
+            "from sys_role ",
+            "where id = #{id}"})
+//    @Select({"select id, role_name roleName, enabled, create_by createBy, create_time createTime from sys_role where id = #{id}"})
+    SysRole selectById(Long id);
 }
